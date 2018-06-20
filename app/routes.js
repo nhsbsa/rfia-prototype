fc0a8e295b3f78139c8890fecfd7977928ff7792
Tim Gorringe <tgorringe4@gmail.com>
2018-06-20 16:20:35 +0100

    created the suggestions from the developer so that they can be ab tested and proven to be unusable

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,9 @@
 var express = require('express');
 var router = express.Router();
 module.exports = router;
+
+function myFunction() {
+    var element = document.getElementById("myDIV");
+    element.classList.remove("hidden");
+}
+
